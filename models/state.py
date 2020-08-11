852e8f71a430c13c7b5e40a2c465d29d40282d09
JuanJoseGomezR <juangomez0149@gmail.com>
2020-08-11 22:27:09 +0000

    [Uploading] Task 7

--- models/state.py
+++ models/state.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
 from models.base_model import BaseModel, Base
 from sqlalchemy import Table, Column, Integer, String
 from models.city import City
