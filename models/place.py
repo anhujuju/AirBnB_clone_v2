413d9b3dbf6a1145bf717435ff4e3aeb0adb7237
JuanJoseGomezR <juangomez0149@gmail.com>
2020-08-12 00:00:09 +0000

    [Fixed] Foreign Keys

--- models/place.py
+++ models/place.py
@@ -7,8 +7,8 @@ from sqlalchemy import Table, Column, Integer, String, ForeignKey
 class Place(BaseModel):
     """ A place to stay """
     __tablename__ = "places"
-    city_id = Column(String(60), nullable=False)
-    user_id = Column(String(60), nullable=False)
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
