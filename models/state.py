1f28534de27e4d72334c67bd1b83440244bfa470
JuanJoseGomezR <juangomez0149@gmail.com>
2020-09-02 16:10:36 +0000

    [Uploading] Task 7

--- models/state.py
+++ models/state.py
@@ -14,10 +14,9 @@ class State(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state", cascade='all, delete')
-    
+
     else:
         name = ""
-
 
         @property
         def cities(self):
