2ee363f9e5847da21d8a1d0c51efb1c490327414
JuanJoseGomezR <juangomez0149@gmail.com>
2020-09-02 15:20:45 +0000

    [Uploading] States

--- models/state.py
+++ models/state.py
@@ -4,20 +4,36 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Table, Column, Integer, String
 from models.city import City
 from sqlalchemy.orm import relationship, backref
+from os import environ
 
 
-class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade='all, delete')
+if "HBNB_TYPE_STORAGE" in environ.keys(
+) and environ["HBNB_TYPE_STORAGE"] == "db":
+    class State(BaseModel, Base):
+        """
+        This is the state class
+        """
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
 
-    @property
-    def cities(self):
-        """cities class"""
-        my_list = {}
-        all_cities = self.cities
-        for city in all_cities:
-            if State.id == city.state_id:
-                list_cities.append(city)
-        return list_cities
+        def __init__(self, **kwargs):
+            setattr(self, "id", str(uuid4()))
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+else:
+    class State(BaseModel, Base):
+        """ State class """
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade='all, delete')
+
+        @property
+        def cities(self):
+            """cities class"""
+            my_list = {}
+            all_cities = self.cities
+            for city in all_cities:
+                if State.id == city.state_id:
+                    list_cities.append(city)
+            return list_cities
