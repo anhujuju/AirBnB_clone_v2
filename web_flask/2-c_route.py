0f0b23e0199f5b67f27704554aa33edfb54fee78
JuanJoseGomezR <juangomez0149@gmail.com>
2020-09-02 15:09:01 +0000

    [Uploading] Tasks

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Running Flask Web App """
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hbnb_greetings():
+    """ Hello HBNB """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ hbnb """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ C Text Replacing """
+    return 'C ' + text.replace("_", " ")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
---
+++ web_flask/4-number_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Running Flask Web App """
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hbnb_greetings():
+    """ Hello HBNB """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ hbnb """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ C Text Replacing """
+    return 'C ' + text.replace("_", " ")
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """ display defined txt """
+    return 'Python ' + text.replace("_", " ")
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Display n is a number if type(n) == int"""
+    if type(n) == int:
+        return "{} is a number".format(n)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
