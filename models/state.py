a4bec5b5826bc6f652bf33a0c9cab1c6e99c98b5
JuanJoseGomezR <juangomez0149@gmail.com>
2020-09-02 15:24:03 +0000

    [Uploading] States

--- models/state.py
+++ models/state.py
@@ -4,29 +4,20 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Table, Column, Integer, String
 from models.city import City
 from sqlalchemy.orm import relationship, backref
-from os import environ
+from os import getenv
+import models
 
 
-if "HBNB_TYPE_STORAGE" in environ.keys(
-) and environ["HBNB_TYPE_STORAGE"] == "db":
-    class State(BaseModel, Base):
-        """
-        This is the state class
-        """
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
-
-        def __init__(self, **kwargs):
-            setattr(self, "id", str(uuid4()))
-            for k, v in kwargs.items():
-                setattr(self, k, v)
-else:
-    class State(BaseModel, Base):
-        """ State class """
-        __tablename__ = "states"
+class State(BaseModel, Base):
+    """ State class """
+    __tablename__ = "states"
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state", cascade='all, delete')
+    
+    else:
+        name = ""
+
 
         @property
         def cities(self):
